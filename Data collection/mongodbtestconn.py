370f2836bc3e4304d6bc6a71d55ffd65990d5ffa
kimgwisoo <kimgs243@gmail.com>
2020-05-12 14:04:31 +0900

    test

--- Data collection/mongodbtestconn.py
+++ Data collection/mongodbtestconn.py
@@ -7,7 +7,7 @@ client = MongoClient("192.168.1.79", 27017)
 db = client.nlu  # DataBase 생성 nlu 이름
 users = db.logging  # collection 생성 logging 이름
 
-mydata = """{"name": "kimyj", "age":26}"""
+mydata = """{"name": "choiym", "age":27}"""
 
 d = json.loads(mydata)
 d['date'] = datetime.datetime.utcnow()
